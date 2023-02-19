9981dafc93f9c8f7859fa24b22bfbcfeecdfdf3d
Adrian <sylzyg555@interia.pl>
2023-02-19 01:33:38 +0100

    TEST
    test

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-const SvgLinks = document.getElementsByClassName("Links-svg-conn");
+const SvgLinks = document.querySelectorAll(".Links-svg-conn")
+// add transition
+var i=0;
+SvgLinks.forEach(element => {
+    element.firstElementChild.addEventListener("onload", (e) => {
+       // console.log(element.firstElementChild);
+       element.firstElementChild.setAttribute("transform","scale(1.3)")
+       element.firstElementChild.setAttribute("fill","#34D9EB")
+       element.lastElementChild.setAttribute("transform","scale(1.2)")
 
-console.log(SvgLinks)
+
+    });
+    element.firstElementChild.addEventListener("mouseout", (e) => {
+       element.firstElementChild.setAttribute("transform","scale(1.0)")
+       element.firstElementChild.setAttribute("fill","#FFFFFF")
+       element.lastElementChild.setAttribute("transform","scale(1.0)")
+       
+    });
+    i++;
+});
+const Top_Circle =document.querySelector(".Top-circle");
+document.addEventListener("DOMContentLoaded", (e) => {
+    //refactor like this other shapes ?
+    // JQuery for Resize > ?
+    Top_Circle.firstElementChild.setAttribute("cx",window.innerWidth);
+    //Top_Circle.firstElementChild.setAttribute("transform", "scale("+window.innerWidth/window.innerHeight+")");
+});
 
