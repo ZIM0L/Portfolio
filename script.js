83e16f05c570c433873ac15edb2e0b339e3e473d
Adrian <sylzyg555@interia.pl>
2023-02-21 21:04:41 +0100

    First-iteration
    Completed (I think so ?) for on mobile.  Other resolution sin the soon.

--- script.js
+++ script.js
@@ -2,27 +2,54 @@ const SvgLinks = document.querySelectorAll(".Links-svg-conn")
 // add transition
 var i=0;
 SvgLinks.forEach(element => {
-    element.firstElementChild.addEventListener("mouseover", (e) => {
-       // console.log(element.firstElementChild);
-       element.firstElementChild.setAttribute("transform","scale(1.3)")
-       element.firstElementChild.setAttribute("fill","#34D9EB")
+       element.firstElementChild.firstElementChild.addEventListener("mouseover", (e) => {
+       element.firstElementChild.firstElementChild.setAttribute("transform","scale(1.3)")
+       element.firstElementChild.firstElementChild.setAttribute("fill","#34D9EB")
        element.lastElementChild.setAttribute("transform","scale(1.2)")
        
 
     });
-    element.firstElementChild.addEventListener("mouseout", (e) => {
-       element.firstElementChild.setAttribute("transform","scale(1.0)")
-       element.firstElementChild.setAttribute("fill","#FFFFFF")
+    element.firstElementChild.firstElementChild.addEventListener("mouseout", (e) => {
+       element.firstElementChild.firstElementChild.setAttribute("transform","scale(1.0)")
+       element.firstElementChild.firstElementChild.setAttribute("fill","#FFFFFF")
        element.lastElementChild.setAttribute("transform","scale(1.0)")
        
     });
     i++;
 });
-const Top_Circle =document.querySelector(".Top-circle");
-document.addEventListener("DOMContentLoaded", (e) => {
-    //refactor like this other shapes ?
-    // JQuery for Resize > ?
-    Top_Circle.firstElementChild.setAttribute("cx",window.innerWidth);
-    //Top_Circle.firstElementChild.setAttribute("transform", "scale("+window.innerWidth/window.innerHeight+")");
-});
+const Project_content = document.querySelectorAll(".Project-content");
+let element_array = [];
+Project_content.forEach( element => {
+element_array.push(element.getElementsByTagName("div")[0].firstElementChild,element.getElementsByTagName("div")[2],element.getElementsByTagName("div")[3]);
+})
+console.log(element_array);
+
+let number;
+const links =document.querySelectorAll('.links');
+links.forEach(element => {
+   element.addEventListener("mouseover",(link) => {
+      number = link.target.dataset.index;
+      console.log(link.target.dataset.index)
+      if (number >= 1 && number <= 2 ) {  //not cool looking, but works ?
+         element_array[0].style.filter = "none"
+            // if (number == 1) {   //scrapped idea
+            //    element_array[1].lastElementChild.style.animation = "arrow-off 1s ease infinite alternate"
+            // }
+      } else if (number >= 3 && number <= 4) {
+         element_array[3].style.filter = "none"
+      } else { 
+         element_array[6].style.filter = "none"
+      }
+   })
+
+   element.addEventListener("mouseout", () => {
+      if (number >= 1 && number <= 2 ) {  //not cool looking, but works ?
+         element_array[0].style.filter = "hue-rotate(50deg)  grayscale(70%)"
+      } else if (number >= 3 && number <= 4) {
+         element_array[3].style.filter = "hue-rotate(50deg)  grayscale(70%)"  
+      } else { 
+         element_array[6].style.filter = "hue-rotate(50deg)  grayscale(70%)"
+      }
+   })
+})
 
